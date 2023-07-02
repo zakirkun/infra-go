c93c9e3bb3b4845430571e88805cdba6d593b493
fanchann <fannyarifnass@gmail.com>
2023-07-02 10:05:03 +0700

    add unit test

---
+++ pkg/auth/jwt_test.go
@@ -0,0 +1,39 @@
+package auth_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/zakirkun/infra-go/pkg/auth"
+)
+
+func TestJWTImpl_GenerateToken(t *testing.T) {
+	jwtImpl := &auth.JWTImpl{
+		SignatureKey: "secretKey",
+		Expiration:   60,
+	}
+
+	data := "Farda Ayu"
+	token, err := jwtImpl.GenerateToken(data)
+
+	fmt.Println(token)
+	assert.NoError(t, err)
+	assert.NotEmpty(t, token)
+}
+
+func TestJWTImpl_ValidateToken(t *testing.T) {
+	jwtImpl := &auth.JWTImpl{
+		SignatureKey: "secretKey",
+		Expiration:   60,
+	}
+
+	data := "Farda Ayu"
+	token, _ := jwtImpl.GenerateToken(data)
+
+	valid, err := jwtImpl.ValidateToken(token)
+
+	assert.NoError(t, err)
+	assert.True(t, valid)
+}
